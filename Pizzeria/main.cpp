93e9ff8c344301fd8cab529e279b1657b7dd58a7
Eduard <terriblegoodday@icloud.com>
2020-04-11 10:43:38 +1000

    Initial Commit

---
+++ Pizzeria/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pizzeria
+//
+//  Created by Eduard Dzhumagaliev on 4/11/20.
+//  Copyright © 2020 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
